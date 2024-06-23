6271d6760fd528e73020aa713297f1ee31159d88
Timothy <timothyaugust1080@gmail.com>
2024-06-23 13:42:52 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,8 +9,9 @@
 int main(void)
 {
         char i;
-        for (i = 'a'; c <= 'z'; i++)
-          putchar(i);
+
+        for (i = 'a' ; i <= 'z'; i++)
+          	putchar(i);
         putchar('\n');
         return (0);
 }
