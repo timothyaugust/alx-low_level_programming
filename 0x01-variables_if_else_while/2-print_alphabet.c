593006957b92b6ff228933914118df40c4b8384d
timothyaugust <146665402+timothyaugust@users.noreply.github.com>
2024-06-23 22:27:35 +0200

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+        char i;
+        for (i = 'a'; c <= 'z'; i++)
+          putchar(i);
+        putchar('\n');
+        return (0);
+}
