8a329ab490701e9d5da2752e4bdfca86c2fb16a8
Timothy <timothyaugust1080@gmail.com>
2024-07-04 12:33:05 -0700

    main.h

---
+++ 0x02-functions_nested_loops/0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *scr, int n);
+int _strcp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+char *rot13(char *a);
+void print_nuber(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void prit_buffer(char *b, int size);
+
+#endif
