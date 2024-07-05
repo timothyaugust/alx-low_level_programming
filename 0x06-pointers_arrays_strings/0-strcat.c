ae6deae76e54f8af3809c6f65a1ea96ac97fcd38
Timothy <timothyaugust1080@gmail.com>
2024-07-05 04:55:10 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include “main.h”
+
+/**
+* _strcat - concatenates two strings
+* @dest: input value
+* @src: input value
+* Return: void
+*/ 
+char *_strat(char *dest, char *src)
+{
+	int i;
+	int js
+	
+	i = 0;
+	while (dest[i] != ‘\0')
+	{
+	i++;
+	}
+	j = 0;
+	while (sc[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j艹；
+	}
+
+	dest[1] = '\0';
+	return (dest);
+}
